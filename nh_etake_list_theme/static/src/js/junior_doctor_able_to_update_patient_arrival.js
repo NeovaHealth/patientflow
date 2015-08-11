ddd9835ffce6654911dd8ed33d8044441ff88c5c
amipatel <amipatel@neovahealth.co.uk>
2015-08-11 15:44:15 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

--- nh_etake_list_theme/static/src/js/junior_doctor_able_to_update_patient_arrival.js
+++ nh_etake_list_theme/static/src/js/junior_doctor_able_to_update_patient_arrival.js
@@ -11,28 +11,33 @@
         id: 'junior_doctor_able_to_update_patient_arrival',
         name: _t("Reconcile the demo bank statement"),
         path: '/web?debug=',
-        mode: 'test',
-
-
+        //mode: 'test',
         steps: [
-
             {
-                title:     _t("Login page Loaded"),
-                element:   '.oe_topbar_name'
-            },
-
-            {
-                title:     _t("Referral Board Loaded"),
-                element:   '.oe_menu_text:contains("Referral Board")'
-            },
-
-            {
-                title:     _t("Notify patient arrival (click on'Arrived'  button)"),
-                element:   'td.oe_kanban_column:nth-child(2) .oe_fold_column.oe_kanban_record:first() button'
+                title:     _t("Login page"),
+                element:   '.oe_topbar_name',
+                popover:   { next: _t("Next")},
+                content:   _t("You are Logged in as Junior Doctor")
             },
             {
-                title:     _t("patient arrival updated and patient is in To be clerked stage"),
-                waitFor:   ('td.oe_kanban_column:nth-child(3) div.oe_fold_column.oe_kanban_record').length + 1
+                title:     _t("Referral Board"),
+                element:   '.oe_menu_text:contains("Referral Board")',
+                popover:   { next: _t("Next")},
+                content:   _t("Referral Board is loaded")
+            },
+            {
+                title:     _t("Click on Arroved Button"),
+                element:   'td.oe_kanban_column:nth-child(2) .oe_fold_column.oe_kanban_record:first() button',
+                popover:   { next: _t("Next")},
+                content: _t("Notify patient arrival by clicking on arrived  button)")
+
+            },
+            {
+               title:     _t("patient arrival updated"),
+               waitFor:   ('td.oe_kanban_column:nth-child(3) div.oe_fold_column.oe_kanban_record').length + 1,
+               popover:   { next: _t("Next"), end: _t('End')},
+               content: _t("patient arrival updated and patient is in To be clerked stage")
+
             }
         ]
     });
