fc65849c83f5561c56d2bbe0bdf0945d9074996e
amipatel <amipatel@neovahealth.co.uk>
2015-08-12 16:43:21 +0100

    [IMP][WI-548] p-flow GUI Testing using tours

--- nh_etake_list_theme/static/src/js/junior_doctor_able_to_update_patient_clerking_in_progress_stage.js
+++ nh_etake_list_theme/static/src/js/junior_doctor_able_to_update_patient_clerking_in_progress_stage.js
@@ -15,35 +15,47 @@
                 popover:   { next: _t("Next")}
             },
             {
-                title:     _t("Referral Board Loaded"),
+                title:     _t("Referral Board"),
+                content:   _t("Referral board is loaded, each patient is presented by index card"),
                 element:   '.oe_menu_text:contains("Referral Board")',
                 popover:   { next: _t("Next")}
             },
             {
-                title:     _t("Clerk patient(click on 'Clerk'  button)"),
+                title:     _t("Clerk patient"),
+                content:   _t("Click on 'Clerk' button to clerk the patient"),
                 element:   'td.oe_kanban_column:nth-child(3) .oe_fold_column.oe_kanban_record:first() button',
                 popover:   { next: _t("Next")}
             },
             {
-                title:     _t("Enter Diagnosis"),
-                element:   '.oe_form_label.oe_align_right label[for=oe-field-input-23]',
+                title:     _t("Diagnosis"),
+                content:   _t("Diagnosis"),
+                element:   'label:contains("Diagnosis:")',
                 popover:   { next: _t("Next")}
             },
             {
                 title:     _t("Enter Plans"),
-                element:   'label[for=oe-field-input-36].oe_form_label.oe_align_right',
+                content:   _t("Enter Plans"),
+                element:   'label:contains("Plan:")',
                 popover:   { next: _t("Next")}
             },
             {
                 title:     _t("Save"),
+                content:   _t("Click on Save button to Save Diagnosis and Plans you have entered"),
                 element:   '.oe_form_button_save',
                 popover:   { next: _t("Next")}
             },
             {
-                title:     _t("patient stage updated to 'clerking in progress stage' "),
+                title:     _t("patient stage updated"),
+                content:   _t(" Patient has moved to 'clerking in progress stage, Click on Referral Board'"),
                 waitFor:   ('td.oe_kanban_column:nth-child(4) div.oe_fold_column.oe_kanban_record').length + 1,
                 popover:   { next: _t("Next")}
-            }
+            },
+            {
+                title:     _t("Referral Board"),
+                content:   _t("Referral board is loaded, each patient is presented by index card"),
+                element:   '.oe_menu_text:contains("Referral Board")',
+                popover:   {next: _t("Next"), end:_t("End")}
+            },
         ]
     });
 }());
